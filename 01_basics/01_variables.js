6816663e28360f228b9eda193a7ac4df047cb1e2
Mohd Asad Alam <72209675+Savagodroid@users.noreply.github.com>
2024-07-07 06:11:30 +0000

    Let, cons variable done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "spengler@gmail.com"
+var accountPassword = "12345"
+accountCity = "Raipur"
+let accountState;
+// accountId = 2  // not allowed
+
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity = "Chennai"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
