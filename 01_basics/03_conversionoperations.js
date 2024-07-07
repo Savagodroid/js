24295d5a8f6056f6797c35cc6c32a2b74249a9ad
Mohd Asad Alam <72209675+Savagodroid@users.noreply.github.com>
2024-07-07 08:01:36 +0000

    conversion

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,26 @@
+let score = "43"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" = 33
+// "33abc" = NaN
+//  true = 1; false = 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 = true ; 0 = false
+
+let someNumber = 43
+
+let stringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof stringNumber);
