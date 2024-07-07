744aff2c49c693dcbfc9829d0d9a3cfe1be99430
Mohd Asad Alam <72209675+Savagodroid@users.noreply.github.com>
2024-07-07 07:15:24 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS Code as newer version
+
+// alert("hello") // we are using nodejs, not browser
+
+console.log(3 + 3)  // code readability should be very high
+ 
+console.log("Spengler") 
+
+
+let name = "spengler"
+let age = 20
+let isLoggedIn = false
+
+
+// number 
+// bigint
+// string
+// boolean
+// null = standalone value
+// undefined 
+// symbol
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
