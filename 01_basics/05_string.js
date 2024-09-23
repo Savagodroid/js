7ecb2258f8f850532074ab05f0f76666ed87f18a
Mohd Asad Alam <72209675+Savagodroid@users.noreply.github.com>
2024-09-23 07:03:37 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,14 @@
+const name = "Savogodroid"
+const repoCount = 50
+
+console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+
+const gameId = new String('Lamar')
+
+console.log(gameId[0]);
+
+
+console.log(gameId.length);
+console.log(gameId.toUpperCase());
