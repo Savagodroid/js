6745c0aaac70b8f7e9270128581206c7bc53f571
Mohd Asad Alam <72209675+Savagodroid@users.noreply.github.com>
2024-09-25 04:01:47 +0000

    nums

---
+++ 01_basics/06_nums_mat.js
@@ -0,0 +1,22 @@
+const score = 400
+
+const balance = new Number(100)
+//console.log(balance);
+
+//onsole.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.6
+
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+// Math
+
+console.log(Math);
+console.log(Math.abs(-3));
+console.log(Math.round(4.3));
+console.log(Math.floor(4.9));
+console.log(Math.ceil(4.5));
