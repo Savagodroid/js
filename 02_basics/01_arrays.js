f78710998607a339f6c0a10ebab01113f9717a6b
Mohd Asad Alam <72209675+Savagodroid@users.noreply.github.com>
2024-10-02 06:56:18 +0000

    1.1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,13 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ["Boogieman", "Nigga"]
+
+// const my Arr2 = new Array(1, 2, 3, 4)
+
+console.log(myArr[4]);
+
+// Array methods
+
+myArr.push(6)
+console.log(myArr);
