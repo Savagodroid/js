9e073042467bfcb9b13a6779a1c268142f4094ab
Mohd Asad Alam <72209675+Savagodroid@users.noreply.github.com>
2024-10-03 21:03:03 +0000

    1.2

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -10,4 +10,16 @@ console.log(myArr[4]);
 // Array methods
 
 myArr.push(6)
+myArr.push(10)
+myArr.pop()
+
+myArr.unshift(9)
+myArr.shift()
+
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(9));
+
+const newArr = myArr.join()
+
 console.log(myArr);
+console.log(newArr);
